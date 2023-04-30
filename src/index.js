4ce4ad7f4cfa191911c3f610b4b35b58f7be208d
Matheus Fernandes Fermiano <matheus.fernandes@mblabs.com.br>
2023-04-30 10:11:27 -0300

    feat: added favicon.ico

--- src/index.js
+++ src/index.js
@@ -2,48 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyles from './styles/GlobalStyles';
-import { createBrowserRouter, RouterProvider  } from 'react-router-dom';
-import Home from './components/Sessions/Home/Home';
-import Contact from './components/Sessions/Contact/Contact';
-import About from './components/Sessions/About/About';
-import Services from './components/Sessions/Services/Services';
-import PhotoGallery from './components/PhotoGallery/PhotoGallery';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: 'home',
-        element: <Home />,
-      },
-      {
-        path: 'about',
-        element: <About />,
-      },
-      {
-        path: 'services',
-        element: <Services />,
-      },
-      {
-        path: 'gallery',
-        element: <PhotoGallery />,
-      },
-      {
-        path: 'contact',
-        element: <Contact />,
-      },
-    ]
-  },
-])
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyles />
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
